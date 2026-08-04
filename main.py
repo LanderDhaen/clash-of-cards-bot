bb023279adc11f12f68fbf48c814d047f0faf4cb
LanderDhaen <lander.dhaen@student.hogent.be>
2026-08-04 18:29:58 +0200

    feat(trade): Added a cancel button

--- main.py
+++ main.py
@@ -27,6 +27,14 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready(): 
     print(f'Ready to roll, {bot.user.name}!')
 
+# Views
+
+class TradeView(discord.ui.View):
+    @discord.ui.button(label="Annuleren", style=discord.ButtonStyle.secondary, emoji="🗑️")
+
+    async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.edit_message(content="Je hebt deze ruil geannuleerd.", embed=None, view=None)
+
 # Commands
 
 @bot.command()
@@ -39,7 +47,7 @@ async def sync(ctx):
 @bot.tree.command(name="trade", description="Wissel kaarten uit voor het Clash of Cards evenement", guild=GUILD)
 async def trade(interaction: discord.Interaction):
     embed = discord.Embed(
-        title="Kaarten uitwisselen",
+        title="Kaarten ruilen voor het Clash of Cards evenement",
         description=(
             "Kaarten op overschot en dringend op zoek naar die laatste kaarten om je set te voltooien? Kijk snel hieronder!\n\n"
             "• Kies de kaart die je wilt weggeven\n"
@@ -49,7 +57,7 @@ async def trade(interaction: discord.Interaction):
         color=discord.Color.orange()
     )
 
-    await interaction.response.send_message(embed=embed, ephemeral=True)
+    await interaction.response.send_message(embed=embed, ephemeral=True, view=TradeView())
 
 
 bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
