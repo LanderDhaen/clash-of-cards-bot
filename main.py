a77b7f34608ef37e3bfb817292de00527ed05707
LanderDhaen <lander.dhaen@student.hogent.be>
2026-08-04 18:28:54 +0200

    feat(trade): Added simple trade command

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
 
+# Load environment variables
+
 load_dotenv()
-token = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = discord.Object(id=os.getenv("GUILD_ID"))
 
 # Logging
 
@@ -24,4 +28,17 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready(): 
     print(f'Ready to roll, {bot.user.name}!')
 
-bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+# Commands
+
+@bot.command()
+async def sync(ctx):
+    await bot.tree.sync(guild=GUILD)
+    await ctx.send('Synced!')
+
+@bot.tree.command(name="trade", description="Wissel kaarten uit met een andere speler", guild=GUILD)
+@app_commands.describe(give="De kaart die je wilt geven", receive="De kaart die je wilt ontvangen")
+async def trade(interaction: discord.Interaction , give: str, receive: str):
+    await interaction.response.send_message(f'{interaction.user.mention} wilt een {give} geven en een {receive} ontvangen.')
+
+
+bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
