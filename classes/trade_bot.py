79c371d573d8c4eff1dc0b28f7dfe36a216b8f0b
LanderDhaen <lander.dhaen@student.hogent.be>
2026-08-04 23:49:23 +0200

    fix(restructuring): Added none check for GUILD

--- classes/trade_bot.py
+++ classes/trade_bot.py
@@ -5,7 +5,7 @@ from config import GUILD, IS_DEVELOPMENT
 
 class TradeBot(commands.Bot):
     async def setup_hook(self):
-        if IS_DEVELOPMENT:
+        if IS_DEVELOPMENT and GUILD is not None:
             self.tree.add_command(trade, guild=GUILD)
             self.tree.clear_commands(guild=None)
 
