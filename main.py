8dc3ce9c984c66973d17269efa8224c0897c049f
LanderDhaen <lander.dhaen@student.hogent.be>
2026-08-04 21:54:02 +0200

    refactor(restructuring): Moved the trade result view to separate file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import logging
 from config import (TOKEN, GUILD, ROLE_ID)
 from data.clans import CLANS
 from data.cards import ELIXIR, DARK_ELIXIR, BUILDER_BASE, SUPER_TROOP
+from views.trade_result import TradeResultView
 
 # Logging
 
@@ -26,39 +27,6 @@ async def on_ready():
     print(f'Ready to roll, {bot.user.name}!')
 
 # Views
-
-class TradeResultView(discord.ui.View):
-    def __init__(self, clan_tag: str, give: list, receive: list):
-        super().__init__(timeout=None)
-
-        self.give = give
-        self.receive = receive
-
-        self.visit_clan_button = discord.ui.Button(label="Bekijk de ruil", style=discord.ButtonStyle.link, url=f"https://link.clashofclans.com/en?action=OpenClanProfile&tag={clan_tag}")
-        self.add_item(self.visit_clan_button)
-
-        self.close_button = discord.ui.Button(label="Afsluiten", style=discord.ButtonStyle.secondary, emoji="🗑️")
-        self.close_button.callback = self.close_button_callback
-        self.add_item(self.close_button)
-
-    # Callbacks
-
-    async def close_button_callback(self, interaction: discord.Interaction):
-
-        embed = discord.Embed(
-            title="Clash of Cards",
-            description=(
-                f"Dit voorstel van {interaction.user.mention} is niet langer beschikbaar:\n"
-                ),
-            color=discord.Color.default()
-        )
-
-        embed.add_field(name="Weggeven", value="\n".join(f"• {card}" for card in self.give), inline=True)
-        embed.add_field(name="Ontvangen", value="\n".join(f"• {card}" for card in self.receive), inline=True)
-
-        await interaction.response.edit_message(embed=embed, view=None, delete_after=60)
-
-
 
 class TradeView(discord.ui.View):
 
