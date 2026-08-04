c5f56dd4cd1ec12fc4175d7fa2f110a25c514670
LanderDhaen <lander.dhaen@student.hogent.be>
2026-08-05 00:51:44 +0200

    feat(restructuring): Implemented improved logging

--- main.py
+++ main.py
@@ -2,29 +2,34 @@ import logging
 import discord
 
 from classes.trade_bot import TradeBot
+from config import TOKEN, IS_DEVELOPMENT
 
-from config import TOKEN, GUILD, IS_DEVELOPMENT
+LOG_LEVEL = logging.DEBUG if IS_DEVELOPMENT else logging.INFO
+LOG_FORMAT = "%(asctime)s %(levelname)-8s %(name)s %(message)s"
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-# Create an instance of the bot
+# Initialize logging
+
+handler = logging.StreamHandler()
+handler.setLevel(LOG_LEVEL)
+formatter = logging.Formatter(LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("trade_bot.main")
+logger.addHandler(handler)
+logger.setLevel(LOG_LEVEL)
+
+# Initialize bot with intents
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 
 bot = TradeBot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name} is online and ready to be used!")
-
-# Create an instance of the logger
-
-handler = logging.StreamHandler()
-level = logging.DEBUG if IS_DEVELOPMENT else logging.INFO
-handler.setLevel(level)
-formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
-handler.setFormatter(formatter)
+    logger.info("%s is online and ready to be used!", bot.user.name)
 
 # Run the bot
 
-bot.run(TOKEN, log_handler=handler)
+bot.run(TOKEN, log_handler=handler, log_level=LOG_LEVEL)
