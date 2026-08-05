e2b41c4a32965ecba3b19291b824ea25ca978c82
LanderDhaen <lander.dhaen@student.hogent.be>
2026-08-05 14:05:19 +0200

    feat(sync): Implemented proper sync commands

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 import discord
 
-from classes.trade_bot import TradeBot
+from discord.ext import commands
+from commands.trade import trade
 from config import TOKEN, IS_DEVELOPMENT
 
 LOG_LEVEL = logging.DEBUG if IS_DEVELOPMENT else logging.INFO
@@ -24,11 +25,34 @@ logger.setLevel(LOG_LEVEL)
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = TradeBot(command_prefix="/", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     logger.info("%s is online and ready to be used!", bot.user.name)
+
+# Commands
+
+import time
+import discord
+from discord.ext import commands
+
+@bot.command(name="sync", help="Sync the bot commands within the guild.")
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: commands.Context):
+    synced = await bot.tree.sync(guild=ctx.guild)
+    await ctx.send(f"Synced {len(synced)} command(s) to {ctx.guild.name}!")
+
+@bot.command(name="release", help="Release the commands globally.")
+@commands.guild_only()
+@commands.is_owner()
+async def release(ctx: commands.Context):
+    synced = await bot.tree.sync()
+    await ctx.send(f"Released {len(synced)} command(s) globally!")
+
+
+bot.tree.add_command(trade)
 
 # Run the bot
 
