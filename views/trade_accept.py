02216ba1ee856c67d8da100b4220d922da6cdb43
LanderDhaen <lander.dhaen@student.hogent.be>
2026-08-12 16:48:57 +0200

    fix(view): Actually add the finish button subclass to the view

--- views/trade_accept.py
+++ views/trade_accept.py
@@ -2,9 +2,9 @@ import discord
 
 from data.clans import Clan
 
-class ConfirmButton(discord.ui.Button):
+class FinishButton(discord.ui.Button):
     def __init__(self):
-        super().__init__(label="Bevestigen", style=discord.ButtonStyle.primary)
+        super().__init__(label="Afronden", style=discord.ButtonStyle.primary)
 
     async def callback(self, interaction: discord.Interaction):
         view = self.view
@@ -13,7 +13,7 @@ class ConfirmButton(discord.ui.Button):
 
         if interaction.user != view.initiator and interaction.user != view.acceptor:
             return await interaction.response.send_message(
-                f"Alleen {view.initiator.mention} of {view.acceptor.mention} kan deze ruil bevestigen.",
+                f"Alleen {view.initiator.mention} of {view.acceptor.mention} kan deze ruil afronden.",
                 ephemeral=True
             )
 
@@ -21,7 +21,7 @@ class ConfirmButton(discord.ui.Button):
             title="Clash of Cards",
             description=(
                 f"{interaction.user.mention} heeft de ruil tussen "
-                f"{view.initiator.mention} en {view.acceptor.mention} bevestigd!\n"
+                f"{view.initiator.mention} en {view.acceptor.mention} afgerond!\n"
             ),
             color=discord.Color.green()
         )
@@ -76,36 +76,10 @@ class TradeAcceptView(discord.ui.View):
         self.acceptor = acceptor
         self.thread = thread
 
-        self.finish_button = discord.ui.Button(label="Afronden",style=discord.ButtonStyle.primary)
-        self.finish_button.callback = self.finish_button_callback
-        self.add_item(self.finish_button)
-
         
+
+        self.add_item(FinishButton())    
         self.add_item(CancelButton())
 
         if self.clan:
             self.add_item(VisitClanButton(self.clan))
-            
-
-    # Callbacks
-
-    async def finish_button_callback(self, interaction: discord.Interaction):
-        if interaction.user == self.initiator or interaction.user == self.acceptor:
-            embed = discord.Embed(
-                title="Clash of Cards",
-                description=(
-                    f"{interaction.user.mention} heeft de ruil tussen "
-                    f"{self.initiator.mention} en {self.acceptor.mention} afgerond!\n"
-                ),
-                color=discord.Color.green()
-            )   
-
-            await interaction.response.edit_message(embed=embed, view=None)
-            await self.thread.edit(archived=True, locked=True)
-
-        else:
-            await interaction.response.send_message(
-                f"Alleen {self.initiator.display_name} of "
-                f"{self.acceptor.display_name} kan deze afronden.",
-                ephemeral=True
-            )
